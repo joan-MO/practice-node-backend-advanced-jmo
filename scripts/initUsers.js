197dde51e7ce49de95e79ca908c7a16b4685d719
Joan M.O <joanmiquelortega@gmail.com>
2021-05-02 15:24:38 +0200

    others

--- scripts/initUsers.js
+++ scripts/initUsers.js
@@ -1,14 +1,13 @@
 'use strict';
 const { Users } = require('../models');
 
-
 module.exports = async function initUsers() {
 
     try {
         const { deletedCount } = await Users.deleteMany();
         console.log(`Deleted ${deletedCount} users.`);
        
-        const users = await Usuario.insertMany([
+        const users = await Users.insertMany([
             {
               email: "admin@example.com",
               password: Users.hashPassword('1234')
